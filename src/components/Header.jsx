10aa2f704ac423f9db898ce467d962e49a723f80
Shi Sha <emailtotheowner@gmail.com>
2020-07-03 16:50:34 -0400

    Heroku deployment done!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
 <header>
       <div className="row px-4 customHeader">
         <div className="col headerColumn">
-          <h1 className="text-center text-dark text-uppercase font-weight-bold mt-5 mb-0"> Employee Directory </h1>
+          <h1 className="text-center text-uppercase font-weight-bold mt-5 mb-0"> Employee Directory </h1>
           <p className="text-center text-secondary mt-0"> <em> A React-based employee directory data search app</em> </p>
         </div>
       </div> 
